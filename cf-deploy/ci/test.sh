4d9e240f38748dd85d1d335c6462becfd74ab90f
J Gregory <john@johngregory.me.uk>
2017-05-23 17:33:02 +0100

    Try some more

--- cf-deploy/ci/test.sh
+++ cf-deploy/ci/test.sh
@@ -5,10 +5,13 @@ set -e -x
 # Shouldn't need to do this!? Go doesn't appear to be respecting the vendor/ path
 # in the main source (although that may be due to how the code is getting loaded
 # into the image)
+# If you want to run the unit tests of the vendor'd libraries themselves then
+# do a `cp -r` instead to leave them locally
 echo "Moving vendor/ to gopath"
 mv gopath/github.com/ONSdigital/sdx-spike/cf-deploy/vendor/* ${GOPATH}/src/
 
 # We're one level down in /ci when we're running this script, so we need to
 # jump back to run the tests.
+echo "Executing tests"
 cd ..
 go test ./...
