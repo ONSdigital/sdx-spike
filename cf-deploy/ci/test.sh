4ed2bca83e9a3f602878c97d6316a00601cec834
J Gregory <john@johngregory.me.uk>
2017-05-23 17:17:49 +0100

    Try some more

--- cf-deploy/ci/test.sh
+++ cf-deploy/ci/test.sh
@@ -2,13 +2,13 @@
 
 set -e -x
 
-echo "Copying vendor/ to gopath"
+# Shouldn't need to do this!? Go doesn't appear to be respecting the vendor/ path
+# in the main source (although that may be due to how the code is getting loaded
+# into the image)
+echo "Moving vendor/ to gopath"
+mv -r gopath/github.com/ONSdigital/sdx-spike/cf-deploy/vendor/* ${GOPATH}/src/
 
-cp -r gopath/github.com/ONSdigital/sdx-spike/cf-deploy/vendor/* ${GOPATH}/src/
-
-ls /go/src/
-
+# We're one level down in /ci when we're running this script, so we need to
+# jump back to run the tests.
 cd ..
-go version
-env
 go test ./...
