ec3a1d6388e4b9fb8d3343ae738204fe08e771ed
J Gregory <john@johngregory.me.uk>
2017-05-23 17:19:29 +0100

    Try some more

--- cf-deploy/ci/test.sh
+++ cf-deploy/ci/test.sh
@@ -6,7 +6,7 @@ set -e -x
 # in the main source (although that may be due to how the code is getting loaded
 # into the image)
 echo "Moving vendor/ to gopath"
-mv -r gopath/github.com/ONSdigital/sdx-spike/cf-deploy/vendor/* ${GOPATH}/src/
+mv gopath/github.com/ONSdigital/sdx-spike/cf-deploy/vendor/* ${GOPATH}/src/
 
 # We're one level down in /ci when we're running this script, so we need to
 # jump back to run the tests.
