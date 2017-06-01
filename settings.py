e8c416be0b40c8637406c7babca3add82e34dbc7
James Gardiner <james@jgardiner.co.uk>
2017-06-01 09:16:16 +0100

    Ready for cf deploy

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ import logging
 LOGGING_FORMAT = "%(asctime)s|%(levelname)s: sdx-spike: %(message)s"
 LOGGING_LEVEL = logging.getLevelName(os.getenv('LOGGING_LEVEL', 'DEBUG'))
 
-PORT= os.getenv('PORT', 8080)
+PORT = int(os.getenv('PORT', 8080))
